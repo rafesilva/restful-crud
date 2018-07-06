3eaa961dbf9d66da92b734c7102d7054609f67e7
Rafael FS <rafesilva@gmail.com>
2018-07-06 20:13:55 +1000

    Add /products route

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const app = express();
 
+const productRoutes = require('./api/routes/products');
 //Middleware
-app.use((req, res, next) => { 
-	res.status(200).json({
-	message: 'It works!'
-
-	}); 
-});
+app.use('/products', productRoutes);
 
 module.exports = app;
