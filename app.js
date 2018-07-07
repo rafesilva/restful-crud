fb96ebe01d9e451937ce12eb7a5147a61cd4d25b
Rafael FS <rafesilva@gmail.com>
2018-07-07 19:27:32 +1000

    Add Errors handler

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
-//Middleware
+const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
+
+
+//Middleware // Routes handling requests
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+//Handling errors
+app.use((req, res, next) => {
+	const error = new Error('Not found');
+	error.status = 404;
+	next(error);
+})
+
+app.use((error, req, res, next) => {
+	res.status(err.status || 500);
+	res.json({
+		error: {
+			message: error.message
+		}
+	});
+});
 
 module.exports = app;
